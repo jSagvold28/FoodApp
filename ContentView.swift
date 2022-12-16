7f945ed7c8e877a50d7a97d7a512675b54767848
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-15 20:20:54 -0600

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  FoodOrdering1
+//
+//  Created by Jayce Sagvold on 12/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        NavigationView {
+            
+    
+            
+            List {
+                
+                NavigationLink {
+                    ChineaseFoodView()
+                } label: {
+                    Text("Chinease Food")
+                }
+                
+                NavigationLink {
+                    Coffee()
+                } label: {
+                    Text("Coffee")
+                }
+                
+                NavigationLink {
+                    KidsMenu()
+                } label: {
+                    Text("Kids Menu")
+                }
+                
+                NavigationLink {
+                    Pizza()
+                } label: {
+                    Text("Pizza")
+                }
+                
+                NavigationLink {
+                    Delevery()
+                } label: {
+    
+                }
+                
+                    
+                
+                    
+                                                
+                }.navigationTitle("Food")
+            }
+        }
+    }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
